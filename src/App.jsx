57a221bf787e2ee10f5ca723fa84be549925dd17
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-11 15:37:35 +0200

    Add Results component and destructure input component and return Results on App.js component with input prop & user input value

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import UserInput from './components/UserInput'
+import Results from './components/Results'
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   )
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Results = ({input}) => {
+  console.log(input)
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Results
